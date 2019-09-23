4a9b590114ac5251e0ea7e1eaf53deff217c88c1
Anderson Chen <ander.chen00@gmail.com>
2019-09-22 21:33:51 -0300

    Update the repos tags

---
+++ src/components/RepoTable/RepoTable.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import classes from './RepoTable.module.css';
+
+
+const RepoTable = (props) => {
+  
+  const createData = (repository, description, language, tags, repo_id) => {
+    return { repository, description, language, tags, repo_id };
+  }
+
+  const rows = props.repos.map(repo => (
+    createData(repo.repo_name, repo.description, repo.language, repo.tags.map(e => e.name).join(', '), repo.repo_id)
+  ))
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Repository</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Language</TableCell>
+            <TableCell>Tags</TableCell>
+            <TableCell>Edit Tags</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.repository}>
+              <TableCell>
+                {row.repository}
+              </TableCell>
+              <TableCell component="th" scope="row">{row.description}</TableCell>
+              <TableCell>{row.language}</TableCell>
+              <TableCell>{row.tags}</TableCell>
+              <TableCell>
+                <button onClick={props.clicked} value={row.repo_id}>Edit</button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+export default RepoTable;
--- src/containers/Repos/Repos.jsx
+++ src/containers/Repos/Repos.jsx
@@ -2,45 +2,92 @@ import React, { Component } from 'react';
 
 import axios from '../../Axios';
 
+import Aux from '../../hoc/Aux/Aux';
 import LoadingBar from '../../components/UI/LoadingBar/LoadingBar';
+import RepoTable from '../../components/RepoTable/RepoTable';
+import Modal from '../../components/UI/Modal/Modal';
+import Input from '../../components/UI/Input/Input';
 
 class Repos extends Component {
   state = {
     username: '',
     repos: [],
-    loading: true
+    loading: true, 
+    showModal: false,
+    repo_id: '',
+    tags: '',
+    id: ''
   }
 
   componentDidMount () {
     const username = this.props.match.params.username
-    axios.get(`/users/${username}/repos`)
-          .then(res => {
-            const fetchedRepos =[];
-            for (let key in res.data) {
-              fetchedRepos.push({...res.data[key],
-              id: key
-              });
-            }
-            this.setState({loading: false, repos: fetchedRepos})
-          })
-          .catch(err => {
-            console.log(err)
-          });
+    this.fetchRepos(username)
+    this.setState({username: username})
   }
+
+  fetchRepos = (username) => {
+    axios.get(`/users/${username}/repos`)
+    .then(res => {
+      const fetchedRepos =[];
+      for (let key in res.data) {
+        fetchedRepos.push({...res.data[key]});
+      }
+      this.setState({loading: false, repos: fetchedRepos})
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  }
+
+  editClickedHandler = (event) => {
+    const repo = this.state.repos.find(repo => repo.repo_id === event.target.value)
+    this.setState({showModal: true, 
+                    repo_id: event.target.value, 
+                    tags: repo.tags.map(e => e.name).join(', '),
+                    id: repo.id})
+
+  }
+
+  closeModalHandler = () => {
+    this.setState({showModal: false, repo_id: ''})
+  }
+
+  onChangeTagHandler = (event) => {
+    const newTag = event.target.value
+    this.setState({tags: newTag})
+  }
+
+  updateTagsHandler = () => {
+    const tags = {tags: this.state.tags.split(", ")}
+    axios.patch(`/users/${this.state.username}/repos/${this.state.id}`, tags)
+          .then(res => {
+            this.setState({showModal: false, repo_id: '', tags: '', id: ''})
+          })
+          .catch(error =>{
+            console.log(error)
+            this.setState({showModal: false, repo_id: '', tags: '', id: ''})
+          });
+
+    this.fetchRepos(this.state.username)
+  }
+
 
   render () {
     let fetchedRepos = null;
-
-    if (this.state.loading) {
-      fetchedRepos = <LoadingBar />
-    } else {
-      fetchedRepos = <h2>yoooooooo</h2>
-    }
+    
+    this.state.loading ? fetchedRepos = <LoadingBar /> : fetchedRepos = <RepoTable repos={this.state.repos} 
+                                                                                        clicked={this.editClickedHandler} />
     return (
-      <div>
+      <Aux>
+        <Modal show={this.state.showModal} modalClosed={this.closeModalHandler}>
+        <form onSubmit={this.updateTagsHandler}>
+          <Input value={this.state.tags} changed={this.onChangeTagHandler}/>
+        </form>
+          <button onClick={this.updateTagsHandler}>Upddate Tags</button>
+        </Modal>
         <h1>This is the repo list</h1>
         {fetchedRepos}
-      </div>
+      </Aux>
     );
   };
 };
